8b8741635690d41d224d9f62f26d8f7f46d34c92
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-02-26 01:00:13 -0500

    Explicitly add fetch to dep list, playing around with api

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,40 @@
+const fetch = require('sketch-polyfill-fetch')
+
 export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+	// const sketch = context.api();
+	// log(sketch.api_version);
+
+  // context.document.showMessage("It's alive 🙌");
+  const doc = context.document;
+  const selection = context.selection;
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(res => {
+  //     context.document.showMessage(res.status);
+  //     return res.json();
+  //   })
+  //   .then(body => {
+  //   	// context.document.showMessage(body.filter(item => item.name));
+  //   	log(body.filter(item => item.name));
+  //   });
+
+  // var openPanel = NSOpenPanel.openPanel()
+  // openPanel.setCanChooseDirectories(false)
+  // openPanel.setCanChooseFiles(true)
+  // openPanel.setCanCreateDirectories(false)
+  // openPanel.setDirectoryURL(NSURL.fileURLWithPath('~/Documents/'))
+
+  // openPanel.setTitle('Choose a file')
+  // openPanel.setPrompt('Choose')
+  // openPanel.runModal()
+
+  // var app = [NSApplication sharedApplication];
+  // [app displayDialog:"This is an alert box!" withTitle:"Alert Box Title"];
+  //
+  // var doc = context.document;
+  // var result = [doc askForUserInput:"How many copies do you want?" initialValue:"10"];
+
+  if (!selection.length) {
+  	doc.showMessage("At Least One Artboard Not Selected");
+    return;
+  }
 }
